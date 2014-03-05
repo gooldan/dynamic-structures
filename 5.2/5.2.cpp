1ef3a1b37fd5dab36eef5b9c02ad6b93b00c9c78
gooldan <egor5700@ya.ru>
2014-03-05 21:23:40 +0400

    stack realization fixed

--- 5.2/5.2.cpp
+++ 5.2/5.2.cpp
@@ -4,66 +4,74 @@
 using namespace std;
 struct stack
 {
-	stack *next, *head;
+	stack *next;
 	char data;
-	int size = 0;
 };
 void Push(char c, stack *&a)
 {
 	stack *t = new stack;
 	t->data = c;
-	t->next = a->head;
-	a->head = t;
-	a->size++;
+	t->next = NULL;
+	if (a!= NULL)
+		t->next = a;
+	a = t;
 }
+
 char Pop(stack *&a)
 {
-	if (a->size == 0)
+	if (a == NULL)
 		return 7;
-	char res = a->head->data;
-	stack *t = a->head;
-	a->head = t->next;
-	delete t;
-	a->size--;
+	char res = a->data;
+	if (a->next == NULL)
+	{
+		delete a;
+		a = NULL;
+		return res;
+	}
+	stack *t = a->next;
+	delete a;
+	a = t;
 	return res;
 }
 void Clean(stack *&a)
 {
-	while (a->size)
+	while (a)
 	{
-		stack *t = a->head;
-		a->head = t->next;
-		delete t;
-		a->size--;
+		if (a->next == NULL)
+		{
+			delete a;
+			continue;
+		}
+		stack *t = a->next;
+		delete a;
+		a = t;
 	}
 }
 bool IsEmpty(stack *&a)
 {
-	return (a->size == 0);
+	return (a == NULL);
 }
 bool IsCorrect(char *s)
 {
 	int len = strlen(s);
 	bool res = true;
 	stack *cup = new stack;
+	cup = NULL;
 	for (int i = 0; i < len; ++i)
 	{
 		if ((s[i] == '}') || (s[i] == ']') || (s[i] == ')'))
 		{
 			char c = Pop(cup);
-			if (s[i] == '}')
-			if (c != '{')
+			if (s[i] == '}' && c != '{')
 				res = false;
-			if (s[i] == ']')
-			if (c != '[')
+			if (s[i] == ']' && c != '[')
 				res = false;
-			if (s[i] == ')')
-			if (c != '(')
+			if (s[i] == ')' && c != '(')
 				res = false;
 			if (!res) break;
 		}
 		else
-		if ((s[i] == '{') || (s[i] == '[') || (s[i] = '('))
+		if ((s[i] == '{') || (s[i] == '[') || (s[i] == '('))
 			Push(s[i], cup);
 	}
 	if (!IsEmpty(cup)) res = false;
@@ -82,5 +90,6 @@ int main()
 	}
 	s[--i] = '\0';
 	cout << IsCorrect(s);
+	delete[]s;
 	cin >> i;
 }
